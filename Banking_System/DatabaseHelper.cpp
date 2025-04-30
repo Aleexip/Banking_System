81e14ee084867f6786168d99e8fbd3d75c8cce0c
Panait Alexandru <aleexip81@gmail.com>
2025-04-30 15:35:17 +0300

    merge

---
+++ Banking_System/DatabaseHelper.cpp
@@ -0,0 +1,37 @@
+#include "DatabaseHelper.h"
+#include <sqlite3.h>
+#include <iostream>
+#include <filesystem>
+
+
+//creates a database if it does not exist
+int createDB(const char* s) {
+    sqlite3* DB;
+    int exit = sqlite3_open(s, &DB);
+
+    sqlite3_close(DB);
+    return 0;
+}
+
+//creates a table if it does not exist
+int createTable(const char* s) {
+    sqlite3* DB;
+
+    // sql definition
+    std::string sql = "CREATE TABLE IF NOT EXISTS ACCOUNTS("
+        "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "NAME TEXT NOT NULL, "
+        "ACCOUNT_NUMBER TEXT NOT NULL UNIQUE, "
+        "BALANCE REAL NOT NULL);";
+
+    int exit = sqlite3_open(s, &DB);
+    char* errorMessage; // variable to hold error message
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &errorMessage); // execute SQL statement
+
+    if (exit != SQLITE_OK) {
+        sqlite3_free(errorMessage); // free error message
+    }
+
+    sqlite3_close(DB); // close database connection
+    return exit;
+}
--- Banking_System/main.cpp
+++ Banking_System/main.cpp
@@ -1,19 +1,33 @@
-#include "LoginForm.h"
-#include "MainDashboard.h"
-#include "TransferService.h"
-#include "Account.h"
-#include "TransactionHistory.h"
-#include "DatabaseManager.h"
 
+?#include "LoginForm.h"
+#include <string>
+#include <iostream>
+
+//for database
+#include "DatabaseHelper.h"
+#include <filesystem>      // for directory creation     
+#include <sqlite3.h>
+
+>>>>>> > Stashed changes
 using namespace System;
 using namespace System::Windows::Forms;
 
+
 int main(array <String^>^ args)
 {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	BankingSystem::LoginForm loginForm;
-	loginForm.ShowDialog();
+    std::filesystem::create_directory("Files"); //create directory "Files" if it does not exist
+    std::string dbPath = "Files/ebanking.db"; // database path
 
-	return 0;
+    //function calls to create database and tables
+    createDB(dbPath.c_str());
+    createTable(dbPath.c_str());
+
+
+    //Graphical User Interface
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    BankingSystem::LoginForm loginForm;
+    loginForm.ShowDialog();
+
+    return 0;
 }
