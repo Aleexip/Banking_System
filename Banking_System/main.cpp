658d8dc4aa0c437a67c6be577dcc5a7855c6ba66
Panait Alexandru <aleexip81@gmail.com>
2025-04-30 14:51:03 +0300

    database_file_creation

--- Banking_System/main.cpp
+++ Banking_System/main.cpp
@@ -1,13 +1,28 @@
-#include "LoginForm.h"
+﻿#include "LoginForm.h"
+#include "MainDashboard.h"
+#include "TransferService.h"
+#include "Account.h"
+#include "TransactionHistory.h"
+#include "DatabaseManager.h"
+
 using namespace System;
 using namespace System::Windows::Forms;
 
 int main(array <String^>^ args)
 {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	BankingSystem::LoginForm loginForm;
-	loginForm.ShowDialog();
+	std::filesystem::create_directory("Files"); //create directory "Files" if it does not exist
+	std::string dbPath = "Files/ebanking.db"; // database path
 
-	return 0;
+	//function calls to create database and tables
+    createDB(dbPath.c_str()); 
+    createTable(dbPath.c_str());
+
+
+	//Graphical User Interface
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    BankingSystem::LoginForm loginForm;
+    loginForm.ShowDialog();
+ 
+    return 0;
 }
